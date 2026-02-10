9d714ae34600c9effa73b507652966a47799c478
Лисенко Софія <146013576+Yosohanomi@users.noreply.github.com>
2026-02-10 13:19:23 +0200

    start of adaptive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,12 @@ export const Header = () => {
     return <>
         <header className={styles.header}>
         <Container>
-            <Logo/>
+        <div className={styles.header__inner}>
+        <Logo/>
+                <input type="checkbox" id="toggle" className={styles.burger__input}/>
+                <label for="toggle" className={styles.burger__label}>
+                    <span className={styles.toggle__btn}></span>
+                </label>
                 <nav className={styles.header__nav}>
                     <ul className={styles.header__list}>
                         <li className={styles.header__item}><a href="#" className={styles.header__link}>Головна</a></li>
@@ -19,13 +24,34 @@ export const Header = () => {
                         <li className={styles.header__item}><a href="#" className={styles.header__link}>Категорії</a></li>
                     </ul>
                 </nav>
+                {/* MOBILE MENU */}
+                <div class={styles.mobile__menu}>
+                                    <nav class={styles.mobile__nav}>
+                                    <ul className={styles.mobile__list}>
+                        <li className={styles.mobile__item}><a href="#" className={styles.mobile__link}>Головна</a></li>
+                        <li className={styles.mobile__item}><a href="#" className={styles.mobile__link}>Товари</a></li>
+                        <li className={styles.mobile__item}><a href="#" className={styles.mobile__link}>Категорії</a></li>
+                    </ul>
+                    </nav>
+                    <div className={styles.mobile__thumb}>
+                        <GrayBtn text='Вхід'/>
+                        <YellowBtn text='Реєстрація'/>
+                    </div>
+                </div>
+
+                <div className={styles.footer__cart}>
                 <div className={styles.header__thumb}>
                     <GrayBtn text='Вхід'/>
                     <YellowBtn text='Реєстрація'/>
+                    
+                    
+                </div>
                     <button className={styles.header__btn}>
                         <img src={shoppingCart} alt="shopping cart" className={styles.header__img} />
                     </button>
                 </div>
+        
+            </div>
         </Container>
         </header>
     </>
